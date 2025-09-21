d60debfa08228551c3fd06d5c3eaf56e336dd243
drop0utboy <cakesniffer3237@gmail.com>
2025-09-21 14:31:41 +0900

    25.09.21. kjg 리뷰 완료

--- src/main/java/com/example/demo/dao/mybatis/ReviewDao.java
+++ src/main/java/com/example/demo/dao/mybatis/ReviewDao.java
@@ -6,8 +6,15 @@ import com.example.demo.dto.Review;
 
 @Mapper
 public interface ReviewDao {
-  public Review selectAllByReviewId();
+  // public Review selectAllByReviewId();
   public Review selectByReviewId(Integer reviewId);
+
+  // Review는 받은 사람 id에 맞는거만 한 번에 불러올 수 있으면 될듯
+  // 작성받은 사람 id로 받은 리뷰 모두 불러오기
+  public Review selectAllByTargetId(Integer targetId);
+  // 모집글에 종속된 리뷰 모두 불러오기
+  public Review selectAllByPostId(Integer rPostId);
+
   public int insert(Review review);
   public int update(Review review);
   public int delete(Integer reviewId);
