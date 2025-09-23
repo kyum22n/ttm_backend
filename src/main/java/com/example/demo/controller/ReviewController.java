22a99c5bbafe67d20a6c84d0dab0c32e2644f236
K <cakesniffer3237@gmail.com>
2025-09-23 10:00:58 +0900

    25.09.23 kjg 리뷰 컨트롤러

---
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Review;
+import com.example.demo.service.ReviewService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/review")
+@Slf4j
+public class ReviewController {
+  @Autowired
+  private ReviewService reviewService;
+
+  // 리뷰 쓰기
+  @PostMapping("/write")
+  public Review reviewWrite(@RequestBody Review review) {
+    log.info(review.toString());
+
+    return reviewService.create(review);
+  }
+
+  // 해당 유저가 받은 리뷰 모두 가져오기
+  @GetMapping("/users/{userId}/reviews")
+  public List<Review> getReceivedReviews(@PathVariable("userId") int userId) {
+    log.info("userId: {}", userId);
+
+    return reviewService.findAllByTargetId(userId);
+  }
+
+}
--- src/main/java/com/example/demo/dao/ReviewDao.java
+++ src/main/java/com/example/demo/dao/ReviewDao.java
@@ -1,6 +1,9 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.dto.Review;
 
@@ -10,11 +13,11 @@ public interface ReviewDao {
 
   // Review는 받은 사람 id에 맞는거만 한 번에 불러올 수 있으면 될듯
   // 작성받은 사람 id로 받은 리뷰 모두 불러오기
-  public Review selectAllByTargetId(Integer targetId);
+  public List<Review> selectAllByTargetId(@Param("targetId") Integer targetId);
   // 모집글에 종속된 리뷰 모두 불러오기
-  public Review selectAllByPostId(Integer rPostId);
+  // public List<Review> selectAllByPostId(@Param ("rPostId") Integer rPostId);
 
   public int insert(Review review);
-  public int update(Review review);
-  public int delete(Integer reviewId);
+  // public int update(Review review); 아무리 생각해도 필요 없음 ㅋㅋ
+  // public int delete(Integer reviewId);
 }
