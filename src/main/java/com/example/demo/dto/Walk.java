4f72aa12a5535a898eee8e8039ad687bb629fea7
kyum22n <aphrod207@gmail.com>
2025-09-24 18:52:48 +0900

    25.09.24 lkm 1:1 산책 구현1

--- src/main/java/com/example/demo/dto/Walk.java
+++ src/main/java/com/example/demo/dto/Walk.java
@@ -3,16 +3,22 @@ package com.example.demo.dto;
 import java.time.LocalDateTime;
 
 public class Walk {
-	// 산책 신청자
-	private Integer participate;
-	// 산책 모집글
-	private Integer postId;
-	// 산책 신청 처리 상태
-	private enum requestStatus{
-		P, A, R, C
-	};
-	// 산책 시작 시간
+
+	// 1:1 산책 신청 건
+	private Integer requestOneId;
+
+	// 1:1 산책 신청자
+	private Integer requestUserId;
+	// 1:1 산책 신청 수락자
+	private Integer receiveUserId;
+	
+	// 1:1 산책 신청 시간
+	private LocalDateTime createdAt;
+	// 1:1 산책 신청 처리 상태
+	private String rStatus;
+	
+	// 1:1 산책 시작 시간
 	private LocalDateTime walkStartedAt;
-	// 산책 종료 시간
+	// 1:1 산책 종료 시간
 	private LocalDateTime walkEndedAt;
 }
